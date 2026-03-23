943d5353e8e5410942e28376cd2513b3fd7d9905
giorno39 <garcovi.izzi@gmail.com>
2026-03-23 22:44:38 +0200

    mark the term paper as cancelled after the teacher unasigns it
    #21

--- final_project/term_papers/views.py
+++ final_project/term_papers/views.py
@@ -751,6 +751,15 @@ def unassign_term_paper(request, pk):
         responded_at=timezone.now(),
     )
 
+    TermPaperRequest.objects.filter(
+        term_paper=term_paper,
+        teacher=request.user,
+        status=TermPaperRequest.StatusChoices.ACCEPTED,
+    ).update(
+        status=TermPaperRequest.StatusChoices.CANCELED,
+        responded_at=timezone.now(),
+    )
+
     term_paper.taken_by = None
     term_paper.save()
 
