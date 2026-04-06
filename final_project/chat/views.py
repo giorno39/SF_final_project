a959432ebfbcebeb5bc37a2a47ffc68f027b4658
giorno39 <garcovi.izzi@gmail.com>
2026-04-07 01:09:52 +0300

    add a start conversation chat with the teacher from lessons details
    #18

--- final_project/chat/views.py
+++ final_project/chat/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Max
-from django.views import generic as views
+from django.db.models import Count, Max
+from django.shortcuts import get_object_or_404, redirect
+from django.views import View, generic as views
 
 from final_project.chat.models import Conversation, Message
+from final_project.lessons.models import Lesson
 
 
 class InboxView(LoginRequiredMixin, views.ListView):
@@ -50,3 +52,27 @@ class ConversationView(LoginRequiredMixin, views.DetailView):
         ).exclude(sender=self.request.user).update(is_read=True)
 
         return context
+
+
+class StartLessonConversationView(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        lesson = get_object_or_404(Lesson.objects.select_related('teacher'), pk=pk)
+        other_user = lesson.teacher
+
+        if request.user == other_user:
+            return redirect('lesson-details', pk=lesson.pk)
+
+        conversation = (
+            Conversation.objects
+            .filter(lesson=lesson, participants=request.user)
+            .filter(participants=other_user)
+            .annotate(num_participants=Count('participants'))
+            .filter(num_participants=2)
+            .first()
+        )
+
+        if not conversation:
+            conversation = Conversation.objects.create(lesson=lesson)
+            conversation.participants.add(request.user, other_user)
+
+        return redirect('chat-conversation', pk=conversation.pk)
